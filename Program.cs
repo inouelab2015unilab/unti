b460a02d1701fa2fbf77d585d7122ef854192169
Rmaavuerlice <rsko-wg-shakhtar-donetsk32517@i.softbank.jp>
2015-04-28 15:53:22 +0900

    gggg

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var n in Enumerable.Range(0, 100)) Console.Write(n);   //Rangeメソッド(Enumerableクラス,Enumerableメソッドの一つ)　 
+            Console.WriteLine("");
+        }
+    }
+}
